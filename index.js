c1fa712bd82b2a0e9edf00fcb607f27e2bee572b
amarsharmamrj <amarsharmamrj@gmail.com>
2023-01-25 20:50:29 +0530

    socket url added by env

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ import multer from 'multer'
 import path from 'path'
 import { fileURLToPath } from 'url';
 
-const socket =  require('socket.io')
-const io = socket(7000, {
-  cors: "http://localhost:3000",
-})
+import { Server } from "socket.io";
+// const io = new Server(7000, {
+//     cors: process.env.CLIENT_URL,
+// });
+// const io = socket(7000, {
+//   cors: "http://localhost:3000",
+// })
+// const io = new Server(7000, {
+//     cors: "http://localhost:3000",
+//   })
 
 import Connection from './src/db/db.js';
 
@@ -20,10 +26,15 @@ import userRoute from './src/routes/users.js'
 import authRoute from './src/routes/auth.js'
 import postRoute from './src/routes/posts.js'
 import conversationRoute from './src/routes/conversations.js'
-import messageRoute from './src/routes/messages.js' 
-import notificationRoute from './src/routes/notifications.js' 
+import messageRoute from './src/routes/messages.js'
+import notificationRoute from './src/routes/notifications.js'
 
 dotenv.config();
+
+// configure socket
+const io = new Server(7000, {
+    cors: process.env.CLIENT_URL,
+});
 
 // database connection
 Connection()
@@ -39,29 +50,29 @@ var corsOptions = {
     optionsSuccessStatus: 200,
     methods: "*"
 }
-app.use(cors(corsOptions)); 
-app.use(function(req, res, next) {
+app.use(cors(corsOptions));
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Cross-Origin-Resource-Policy', 'cross-origin');
     next();
-});  
+});
 
 // file upload
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "public/images");
+        cb(null, "public/images");
     },
     filename: (req, file, cb) => {
-      cb(null, req.body.name);
+        cb(null, req.body.name);
     },
-  });
+});
 const upload = multer({ storage: storage });
 app.post("/api/upload", upload.single("file"), (req, res) => {
     try {
-      return res.status(200).json("File uploded successfully");
+        return res.status(200).json("File uploded successfully");
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
 });
 
@@ -86,7 +97,7 @@ let users = []
 
 const addUser = (userId, socketId) => {
     !users.some((user) => user.userId === userId) &&
-        users.push({userId, socketId})
+        users.push({ userId, socketId })
 }
 
 const removeUser = (socketId) => {
@@ -98,9 +109,9 @@ const getUser = (userId) => {
 }
 
 io.on("connection", (socket) => {
-    console.log("a user connected") 
+    console.log("a user connected")
     io.emit("welcome", "hello welcome to social buddy char application")
-    
+
     // on connect
     socket.on("addUser", (userId) => {
         addUser(userId, socket.id)
@@ -108,9 +119,9 @@ io.on("connection", (socket) => {
     })
 
     // send and get message
-    socket.on("sendMessageDeleted", ({senderId, receiverId, messageId}) => {
+    socket.on("sendMessageDeleted", ({ senderId, receiverId, messageId }) => {
         const user = getUser(receiverId)
-        if(user){
+        if (user) {
             io.to(user.socketId).emit("getMessageDeleted", {
                 senderId, messageId
             })
@@ -118,9 +129,9 @@ io.on("connection", (socket) => {
     })
 
     // send and get message deleted
-    socket.on("sendMessage", ({senderId, receiverId, text, image}) => {
+    socket.on("sendMessage", ({ senderId, receiverId, text, image }) => {
         const user = getUser(receiverId)
-        if(user){
+        if (user) {
             io.to(user.socketId).emit("getMessage", {
                 senderId, text, image
             })
@@ -128,7 +139,7 @@ io.on("connection", (socket) => {
     })
 
     // router connect
-    socket.on("routerConnect", ({data}) => {
+    socket.on("routerConnect", ({ data }) => {
         console.log("routerConnect:", data)
         // const user = getUser(receiverId)
         // if(user){
@@ -139,31 +150,31 @@ io.on("connection", (socket) => {
     })
 
     // typing
-    socket.on("typing", ({receiverId, typing, conversationId}) => {
+    socket.on("typing", ({ receiverId, typing, conversationId }) => {
         const user = getUser(receiverId)
-        if(user){
+        if (user) {
             io.to(user.socketId).emit("typing", {
                 typing, receiverId, conversationId
             })
         }
     })
-    
+
     // on disconnect
-    socket.on("disconnect", ()=> {
-        console.log("a user connected") 
-        removeUser(socket.id)  
+    socket.on("disconnect", () => {
+        console.log("a user connected")
+        removeUser(socket.id)
         io.emit("getUsers", users)
     })
 
 
     // hi
-    socket.on("hi", ({username}) => {
+    socket.on("hi", ({ username }) => {
         socket.broadcast.emit("hey", username)
     })
 })
 // messenger
- 
-const PORT = process.env.port || 5000 
+
+const PORT = process.env.port || 5000
 
 app.get("/home", (req, res) => {
     res.send("<h1>home api</h1>")
